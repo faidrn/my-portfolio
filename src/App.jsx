955e464e5b326943c9e4e83264940ee8548498e3
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-10-25 12:38:47 +0200

    feat: Add about component, still no coding

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React,{ useState } from 'react'
 import { LanguageProvider } from './contexts/LanguageContext.jsx';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import About from './components/sections/About';
 //import './App.css'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <LanguageProvider>
         <div className='min-h-screen bg-white'>
           <Header />
-          <main className='flex-grow'>
+          <main>
+            <About />
             {/* Main content goes here */}
             <div className='container mx-auto px-4 py-8'>
               <h1 className='text-3xl font-bold mb-4'>Welcome to My Portfolio</h1>
